97d89ae09e3f5c2ff45a4564f03fe5b920d3f01f
HTaniguchi <hisashi-miyoshi@ckd.co.jp>
2025-07-24 23:13:04 +0900

    Fix import paths for P105Plug in p100-plug and p110-plug files to ensure correct module resolution.

--- src/devices/plugs/p110-plug.ts
+++ src/devices/plugs/p110-plug.ts
@@ -1,4 +1,4 @@
-import { P105Plug } from './_p105-plug';
+import { P105Plug } from './p105-plug';
 import { TapoCredentials, TapoApiRequest, P105UsageInfo, Result, FeatureNotSupportedError, DeviceCapabilityError } from '../../types';
 
 /**
